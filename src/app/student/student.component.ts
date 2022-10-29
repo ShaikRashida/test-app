a1eb638055f306448c65f71243c4011a82abd4b9
ShaikRashida <skkarishma9895@gmail.com>
2022-10-29 12:33:40 +0530

    initial commit

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+   
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  
+  }
+  
+
+}
